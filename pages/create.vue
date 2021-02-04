a07f4341f0c05120dd1a5e944fdd6bee17613d44
James <james.lee@biji.co>
2021-02-04 17:43:54 +0800

    vue-meta done

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,20 @@
     <h1>Created an Event</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create Listing',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create you event in your neighborhood',
+        },
+      ],
+    }
+  },
+}
+</script>
