96b0bde1d29dba670bb0508b6218699c063d628d
a9261020 <fish840212@yahoo.com.tw>
2021-02-12 01:33:20 +0800

    fix: baseURL

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000',
+  baseURL: `http://my-json-server.typicode.com/Code-Pop/real-world-nuxt/`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
